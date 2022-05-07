fe44410b82d5a0c0db9a343813390754a698605a
Eliúde Vemba <sweeteliude@gmail.com>
2022-05-07 02:30:45 +0100

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="text-center">
+          Pokedex 2022
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex flex-center">
+    <h2>{{ name }}</h2>
+    <q-img :src="url" :ratio="1" width="250px" />
+
+    <div class="row justify-around full-width">
+      <q-input filled v-model="id" label="Pesquise por um pokemon" />
+      <q-btn color="red" label="Pesquisar" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import api from "src/services/api";
+
+export default {
+  name: 'IndexPage',
+
+  data () {
+    return {
+      name: "",
+      url: "",
+      id: 1
+    };
+  },
+
+  beforeMount() {
+    api.get('/pokemon/2')
+    .then((response) => {
+      console.log(response);
+      this.name = response.data.name;
+      this.url = response.data.sprites.other.dream_world.front_default;
+      console.log(this.url);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+    });
+  }
+}
+</script>
